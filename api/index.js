622711dab9022a968b77921b08533caa12eb45b3
JoaquinManchenoP <manchenojj@g.cofc.edu>
2021-12-27 11:48:17 -0500

    User Posts

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/post');
 
 dotenv.config();
 const app = express();
@@ -13,13 +14,13 @@ mongoose.connect(process.env.MONGO_URL, () => {
     console.log('Connection to MongoDB successfull')
 });
 
-
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/posts' , postRoute);
 
 app.listen(8000, () => {
     console.log('Backend server running');
